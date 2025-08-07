e51b6284fe278c6db3d28ce5f63cad5737622609
Bonghee <hae8064@naver.com>
2025-08-07 22:11:33 +0900

    fix: Home컴포넌트 스타일 구조 변경

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,10 +1,20 @@
+"use client";
+
 import React from "react";
 import Main from "./Main";
+import AboutMe from "./AboutMe";
 
 const Home = () => {
   return (
-    <div className="sticky top-0 z-50 w-screen h-screen">
-      <Main />
+    <div className="flex flex-col w-screen h-screen relative">
+      <div className="fixed top-0 w-full h-full">
+        <Main />
+      </div>
+      <div
+        className={`absolute top-full w-full h-full transition-all duration-1000 ease-out`}
+      >
+        <AboutMe />
+      </div>
     </div>
   );
 };
